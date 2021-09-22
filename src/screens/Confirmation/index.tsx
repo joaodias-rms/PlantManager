b7b6febad1e608701888321469dd24657e3297d2
joao <jvd.ramos@hotmail.com>
2021-09-22 17:01:38 -0400

    Criada tela de confirmação de username

---
+++ src/screens/Confirmation/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+
+import { Button } from "../../components/Button";
+import {styles} from './styles'
+export function Confirmation() {
+    return (
+        <SafeAreaView>
+            <Text></Text>
+        </SafeAreaView>
+    )
+}
