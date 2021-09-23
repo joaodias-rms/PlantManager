1d5df132463fc6d65b0f2fb73f1bdf667b98a324
joao <jvd.ramos@hotmail.com>
2021-09-23 15:13:33 -0400

    estilizado componentes

---
+++ src/screens/PlantSelect/styles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native"
+import colors from "../../styles/colors"
+import fonts from "../../styles/fonts"
+
+export const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: colors.background
+    },
+    header:{
+        paddingHorizontal: 30
+    },
+    title:{
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle:{
+        fontFamily: fonts.text,
+        fontSize: 17,
+        lineHeight: 20,
+        color: colors.heading
+    },
+    environmentList:{
+      height: 40,
+      paddingBottom: 5,
+      justifyContent: 'center',
+      marginLeft: 32,
+      marginVertical: 32
+    },
+    plants:{
+        flex: 1,
+        paddingHorizontal: 32,
+        justifyContent: 'center'
+    }
+})
