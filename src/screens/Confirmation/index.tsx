617722c5c2b7b66d6dcfe0d46ac63f91fe699b26
joao <jvd.ramos@hotmail.com>
2021-09-23 15:15:25 -0400

    Adicionado navegação na tela de confirmação

--- src/screens/Confirmation/index.tsx
+++ src/screens/Confirmation/index.tsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "../../components/Button";
-import {styles} from './styles'
+
+import { styles } from "./styles";
+
 export function Confirmation() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.content}> 
-                <Text style={styles.emoji}>
-                    😁
-                </Text>
-                <Text style={styles.title}>
-                    Prontinho
-                </Text>
-                <Text style={styles.subtitle}>
-                    Agora vamos começar a cuidar das suas plantinhas com muito cuidado
-                </Text>
-            <View style={styles.footer}>
-            <Button title="Começar"/>
-            </View>
-            </View>
-        </SafeAreaView>
-    )
+  const navigation = useNavigation();
+
+  const handleMoveOn = () => {
+    navigation.navigate("PlantSelect");
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>😁</Text>
+        <Text style={styles.title}>Prontinho</Text>
+        <Text style={styles.subtitle}>
+          Agora vamos começar a cuidar das suas plantinhas com muito cuidado
+        </Text>
+        <View style={styles.footer}>
+          <Button title="Começar" onPress={handleMoveOn} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
