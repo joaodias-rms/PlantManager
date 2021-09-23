dae15bea10c813a57d197ff89713221203719ae0
joao <jvd.ramos@hotmail.com>
2021-09-23 15:14:00 -0400

    refatorado rotas

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-
 import { createStackNavigator } from "@react-navigation/stack";
-import colors from "../styles/colors";
+
 import { Welcome } from "../screens/welcome";
 import { UserIdentification } from "../screens/UserIdentification";
+import { PlantSelect } from "../screens/PlantSelect";
 import { Confirmation } from "../screens/Confirmation";
+
+import colors from "../styles/colors";
 
 const stackRoutes = createStackNavigator();
 
@@ -16,20 +18,25 @@ const AppRoutes: React.FC = () => (
       },
     }}
   >
-    <stackRoutes.Screen 
-        name="Welcome" 
-        component={Welcome} 
-        options={{headerShown: false}}
+    <stackRoutes.Screen
+      name="Welcome"
+      component={Welcome}
+      options={{ headerShown: false }}
     />
     <stackRoutes.Screen
-        name="UserIdentification"
-        component={UserIdentification}
-        options={{headerShown: false}}
+      name="UserIdentification"
+      component={UserIdentification}
+      options={{ headerShown: false }}
     />
-    <stackRoutes.Screen 
-        name="Confirmation" 
-        component={Confirmation} 
-        options={{headerShown: false}}
+    <stackRoutes.Screen
+      name="Confirmation"
+      component={Confirmation}
+      options={{ headerShown: false }}
+    />
+    <stackRoutes.Screen
+      name="PlantSelect"
+      component={PlantSelect}
+      options={{ headerShown: false }}
     />
   </stackRoutes.Navigator>
 );
