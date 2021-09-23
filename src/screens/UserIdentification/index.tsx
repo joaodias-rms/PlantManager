e77cd8ded8374775cac7158c1aede93795afa2db
joao <jvd.ramos@hotmail.com>
2021-09-23 09:57:09 -0400

    Refatorado para uso de rotas

--- src/screens/UserIdentification/index.tsx
+++ src/screens/UserIdentification/index.tsx
@@ -5,6 +5,8 @@ import {
   View,
   TextInput,
   KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
   Platform,
 } from "react-native";
 
@@ -13,10 +15,14 @@ import colors from "../../styles/colors";
 
 import { styles } from "./styles";
 
+import { useNavigation } from "@react-navigation/native";
+
 export function UserIdentification() {
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
   const [name, setName] = useState<string>();
+
+  const navigation = useNavigation();
 
   const handleInputBlur = () => {
     setIsFocused(false);
@@ -32,33 +38,41 @@ export function UserIdentification() {
     setName(value);
   };
 
+  const handleSubmit = () => {
+    navigation.navigate("Confirmation");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
         style={styles.container}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        <View style={styles.content}>
-          <View style={styles.form}>
-            <View style={styles.header}>
-              <Text style={styles.emoji}>{isFilled ? '😄' : '😃'}</Text>
-              <Text style={styles.title}>Como podemos {`\n`} chamar você?</Text>
-            </View>
-            <TextInput
-              style={[
-                styles.input,
-                (isFocused || isFilled) && { borderColor: colors.green },
-              ]}
-              placeholder="Digite um nome"
-              onBlur={handleInputBlur}
-              onFocus={handleInputFocus}
-              onChangeText={handleInputChange}
-            />
-            <View style={styles.footer}>
-              <Button title="Confirmar" />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <View style={styles.header}>
+                <Text style={styles.emoji}>{isFilled ? "😄" : "😃"}</Text>
+                <Text style={styles.title}>
+                  Como podemos {`\n`} chamar você?
+                </Text>
+              </View>
+              <TextInput
+                style={[
+                  styles.input,
+                  (isFocused || isFilled) && { borderColor: colors.green },
+                ]}
+                placeholder="Digite um nome"
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
+              <View style={styles.footer}>
+                <Button title="Confirmar" onPress={handleSubmit} />
+              </View>
             </View>
           </View>
-        </View>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -6,12 +6,20 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
+import { Feather } from "@expo/vector-icons";
 import wateringImg from "../../assets/watering.png";
+
 import { styles } from "./styles";
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  const handleStart = () => {
+    navigation.navigate("UserIdentification");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.wraper}>
@@ -23,7 +31,11 @@ export function Welcome() {
           Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar sempre
           que você precisar.
         </Text>
-        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
           <Feather name="chevron-right" style={styles.buttonIcon} />
         </TouchableOpacity>
       </View>
