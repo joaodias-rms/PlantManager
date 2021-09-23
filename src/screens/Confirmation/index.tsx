9492cd0b4b914a5ceb000398ffce9338a9776ad2
joao <jvd.ramos@hotmail.com>
2021-09-23 09:57:20 -0400

    Criada tela de confirmação de username

--- src/screens/Confirmation/index.tsx
+++ src/screens/Confirmation/index.tsx
@@ -5,8 +5,21 @@ import { Button } from "../../components/Button";
 import {styles} from './styles'
 export function Confirmation() {
     return (
-        <SafeAreaView>
-            <Text></Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}> 
+                <Text style={styles.emoji}>
+                    😁
+                </Text>
+                <Text style={styles.title}>
+                    Prontinho
+                </Text>
+                <Text style={styles.subtitle}>
+                    Agora vamos começar a cuidar das suas plantinhas com muito cuidado
+                </Text>
+            <View style={styles.footer}>
+            <Button title="Começar"/>
+            </View>
+            </View>
         </SafeAreaView>
     )
 }
