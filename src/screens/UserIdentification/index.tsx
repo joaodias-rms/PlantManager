8d3936b556cfddffc6d94bb1114ed7bfdf7c8b98
joao <jvd.ramos@hotmail.com>
2021-09-24 17:22:24 -0400

    refatorado componente para tratamento de exceção

--- src/screens/UserIdentification/index.tsx
+++ src/screens/UserIdentification/index.tsx
@@ -13,13 +13,10 @@ import {
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-
 import { Button } from "../../components/Button";
 
 import { styles } from "./styles";
 import colors from "../../styles/colors";
-
 
 export function UserIdentification() {
   const [isFocused, setIsFocused] = useState(false);
@@ -43,12 +40,14 @@ export function UserIdentification() {
   };
 
   const handleSubmit = async () => {
-    if (!name) 
-      return Alert.alert("Me diz como chamar você 😢");
+    if (!name) return Alert.alert("Me diz como chamar você 😢");
 
-    await AsyncStorage.setItem("@plantmanager:user", name)
-
-    navigation.navigate("Confirmation");
+    try {
+      await AsyncStorage.setItem("@plantmanager:user", name);
+      navigation.navigate("Confirmation");
+    } catch {
+      return Alert.alert("Não foi possível salvar o nome do usuário😢");
+    }
   };
 
   return (
