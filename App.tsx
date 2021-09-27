791a39538baad11d1fc0d843b6ceb5a97fc020a7
joao <jvd.ramos@hotmail.com>
2021-09-27 16:44:17 -0400

    Adicionado listerner de notificações

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppLoading from "expo-app-loading";
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
 } from "@expo-google-fonts/jost";
+import * as Notifications from "expo-notifications";
 
 import Routes from "./src/routes";
-
+import { PlantProps } from "./src/libs/storage";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,10 +16,27 @@ export default function App() {
     Jost_600SemiBold,
   });
 
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async (notification) => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      }
+    );
+
+    return () => subscription.remove();
+    // async function notification(){
+    //   const data = await Notifications.getAllScheduledNotificationsAsync()
+    //   console.log(data);
+      
+    // }
+    // notification()
+
+  }, []);
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
 
-  return <Routes />
-
+  return <Routes />;
 }
