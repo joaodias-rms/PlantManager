0ffa6482149dc3a0250f8dbf6157311aa5bba1aa
joao <jvd.ramos@hotmail.com>
2021-09-27 16:42:16 -0400

    refatorado componente para remoção de plants

--- src/components/PlantCardSecondary/index.tsx
+++ src/components/PlantCardSecondary/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Animated } from "react-native";
+
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
 import { SvgFromUri } from "react-native-svg";
-import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
 
+import colors from "../../styles/colors";
 import { styles } from "./styles";
 
 type PlantsProps = RectButtonProps & {
@@ -12,17 +16,34 @@ type PlantsProps = RectButtonProps & {
     photo: string;
     hour: string;
   };
+  handleRemove: ()=> void;
 };
 
-export function PlantCardSecondary({ data, ...rest }: PlantsProps) {
+export function PlantCardSecondary({ data, handleRemove, ...rest }: PlantsProps) {
   return (
-    <RectButton style={styles.container} {...rest}>
-      <SvgFromUri uri={data.photo} width={50} height={50} />
-      <Text style={styles.title}>{data.name}</Text>
-      <View style={styles.details}>
-        <Text style={styles.timeLabel}>Regar às</Text>
-        <Text style={styles.time}>{data.hour}</Text>
-      </View>
-    </RectButton>
+    <Swipeable
+      overshootRight={false}
+      renderRightActions={() => (
+        <Animated.View>
+          <View>
+            <RectButton
+              style={styles.buttonRemove}
+              onPress={handleRemove}
+            >
+              <Feather name="trash" size={32} color={colors.white}/>
+            </RectButton>
+          </View>
+        </Animated.View>
+      )}
+    >
+      <RectButton style={styles.container} {...rest}>
+        <SvgFromUri uri={data.photo} width={50} height={50} />
+        <Text style={styles.title}>{data.name}</Text>
+        <View style={styles.details}>
+          <Text style={styles.timeLabel}>Regar às</Text>
+          <Text style={styles.time}>{data.hour}</Text>
+        </View>
+      </RectButton>
+    </Swipeable>
   );
 }
